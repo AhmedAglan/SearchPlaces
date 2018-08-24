33d3d60c2907c540f8c509ddb79d826b3e12af7b
Ahmed Aglan <aglan@live.com>
2018-08-24 10:50:16 +0200

    Initial commit

---
+++ src/Components.cs
@@ -0,0 +1,25 @@
+﻿namespace SearchPlaces
+{
+    /// <summary>
+    /// The Components object enables filtering locations for PlacesBar Google Places API requests
+    /// The components parameter of the google places API allows restricting results to specific countries
+    /// </summary>
+    public class Components
+    {
+        private string components;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="T:SearchPlaces.Components"/> class
+        /// </summary>
+        /// <param name="components">A components string as per the google places API (eg. contry:au|country=nz)</param>
+        public Components(string components)
+        {
+            this.components = components;
+        }
+
+        public override string ToString()
+        {
+            return $"&components={components}";
+        }
+    }
+}
